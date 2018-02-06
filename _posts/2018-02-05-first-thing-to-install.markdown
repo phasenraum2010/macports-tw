1c9ce9eb8733ebd6ed14d3c29d45e4af8f884ee3
phasenraum2010 <thomas@woehlke.org>
2018-02-06 01:27:24 +0100

    work in progress

--- _posts/2018-02-05-first-thing-to-install.markdown
+++ _posts/2018-02-05-first-thing-to-install.markdown
@@ -1,11 +1,59 @@
 ---
 layout: post
-title:  "Installing Mamp Server for TYPO3"
+title:  "First macports to be installed in the beginning"
 date:   2018-02-05 13:45:50 +0100
 categories: jekyll update
 ---
 
 # First macports to install in the beginning
 
-sudo port install git
+## Start with updating
+
+```bash
+sudo port -v selfupdate
+sudo port upgrade outdated
+```
+
+## Python
+
+```bash
+sudo port install python27
+sudo port select --set python python27
+sudo port select --set python2 python27
+sudo port install py27-matplotlib py27-numpy py27-scipy py27-ipython +notebook
+```
+
+```bash
+sudo sudo port select --set ipython py27-ipython
+sudo port select --set ipython2 py27-ipython
+sudo port install py27-notebook
+sudo port install opencv +python27
+sudo port install py27-pip
+```
+
+## Ruby for jekyll
+
+```bash
+sudo port install ruby25
+sudo port select ruby ruby25
+sudo port install rb-rubygems
+sudo port install rbenv ruby-build
+```
+
+## Git, subversion, ...
+
+```bash
+sudo port install colorsvn p5.26-svn-mirror svndigest
+sudo port install subversion-perlbindings subversion-python27bindings
+sudo port install subversion-rubybindings
+sudo port install git cgit github-backup
 sudo port install vim mpvim rails.vim p5.26-text-vimcolor
+sudo gem install jekyll bundler
+```
+
+## Video
+
+```bash
+sudo port install VLC +dbus +freerdp +huge +jack +pulse +qt5 +shout +smb +svg -X11
+sudo port install youtube-dl +ffmpeg +python27
+```
--- first-macports/install-basics.bash
+++ first-macports/install-basics.bash
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-sudo port -v selfupdate
-sudo port upgrade outdated
-sudo port install python27
-sudo port select --set python python27
-sudo port select --set python2 python27
-sudo port install py27-matplotlib py27-numpy py27-scipy py27-ipython +notebook
-sudo sudo port select --set ipython py27-ipython
-sudo port select --set ipython2 py27-ipython
-sudo port install py27-notebook
+if false; then
+  sudo port -v selfupdate
+  sudo port upgrade outdated
+fi
 
-sudo port install ruby25
-sudo port select ruby ruby25
-sudo port rb-rubygems
-sudo port install rbenv ruby-build
+if false; then
+  sudo port install python27
+  sudo port select --set python python27
+  sudo port select --set python2 python27
+  sudo port install py27-matplotlib py27-numpy py27-scipy py27-ipython +notebook
+  sudo sudo port select --set ipython py27-ipython
+  sudo port select --set ipython2 py27-ipython
+  sudo port install py27-notebook
+  sudo port install opencv +python27
+  sudo port install py27-pip
+fi
 
-sudo port install colorsvn cvs2svn p5.26-svn-mirror websvn svndigest subversion-perlbindings subversion-python27bindings subversion-rubybindings
-sudo port install git cgit git-cvs github-backup svn2git
-sudo port install vim mpvim rails.vim p5.26-text-vimcolor
+if true; then
+  sudo port install ruby25
+  sudo port select ruby ruby25
+  sudo port install rb-rubygems
+  sudo port install rbenv ruby-build
+fi
 
-sudo gem install jekyll bundler
+if true; then
+  sudo port install colorsvn p5.26-svn-mirror svndigest subversion-perlbindings subversion-python27bindings subversion-rubybindings
+  sudo port install git cgit github-backup
+  sudo port install vim mpvim rails.vim p5.26-text-vimcolor
+fi
+
+if false; then
+  sudo gem install jekyll bundler
+fi
